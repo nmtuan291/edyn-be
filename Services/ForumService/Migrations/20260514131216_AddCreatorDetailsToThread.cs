7745b3f0c94b37c1feb8503cd8a8957607ed00c2
nmtuan291 <minhtuan034015@gmail.com>
2026-05-14 20:13:33 +0700

    feat(forum): add database migration for creator details and fix build

---
+++ Services/ForumService/Migrations/20260514131216_AddCreatorDetailsToThread.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ForumService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatorDetailsToThread : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatorAvatar",
+                table: "Threads",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatorName",
+                table: "Threads",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatorAvatar",
+                table: "Threads");
+
+            migrationBuilder.DropColumn(
+                name: "CreatorName",
+                table: "Threads");
+        }
+    }
+}
